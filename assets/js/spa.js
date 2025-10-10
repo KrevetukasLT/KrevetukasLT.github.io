2d746021b42247113f7cf82cffaaf7de022143bb
KrevetukasLT <215874137+KrevetukasLT@users.noreply.github.com>
2025-10-10 23:13:27 +0300

    renamed template to style

--- assets/js/spa.js
+++ assets/js/spa.js
@@ -112,11 +112,11 @@ document.addEventListener('DOMContentLoaded', async () => {
 
             while (currentPath && currentPath.length >= C.BASE_DIR.length)
             {
-                const template = await fetchHtmlAsDom(currentPath + '/template.html');
-                if (template) {
-                    formatPage(template.body, fills);
-                    template.querySelector(C.CONTENT_SELECTOR).innerHTML = doc.innerHTML;
-                    doc = template.body;
+                const style = await fetchHtmlAsDom(currentPath + '/style.html');
+                if (style) {
+                    formatPage(style.body, fills);
+                    style.querySelector(C.CONTENT_SELECTOR).innerHTML = doc.innerHTML;
+                    doc = style.body;
                 }
 
                 if (currentPath === C.BASE_DIR) {
